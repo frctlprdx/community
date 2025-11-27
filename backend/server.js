90d1d850d6ba83808ffedbe52c3f5dc2d0202e4f
frctlprdx <ifam4@yahoo.com>
2025-11-27 20:36:48 +0700

    Add Cors

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,39 @@
-const app = require("./app");
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
 const express = require("express");
-
+const cors = require("cors");
+const app = require("./app");
 const authRoutes = require("./src/routes/authRoutes");
 const userRoutes = require("./src/routes/userRoutes");
 const postRoutes = require("./src/routes/galleryRoutes");
 const communityRoutes = require("./src/routes/communityRoutes");
 const eventRoutes = require("./src/routes/eventRoutes");
+const { PrismaClient } = require("@prisma/client");
 
+const prisma = new PrismaClient();
 const PORT = process.env.PORT || 3000;
 
+// Allowed origins
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://community-bice.vercel.app"
+];
+
+// CORS middleware
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/gallery", postRoutes);
@@ -19,5 +41,5 @@ app.use("/api/community", communityRoutes);
 app.use("/api/event", eventRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
